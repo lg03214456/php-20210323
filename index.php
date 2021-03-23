54017af02bc365f2602efb8650cb3685ea6db7b8
lg03214456 <lg03214456@gmail.com>
2021-03-23 17:19:37 +0800

    first commit

--- index.php
+++ index.php
@@ -1,28 +1,32 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>何敏煌的練習網頁</title>
+	<title>Welcome to ChengHanYang website</title>
 	<style>
 		body {
-			  background-color: lightblue;
+			  font-family: 微軟正黑體;
 			}
 
-			h1 {
-			  color: white;
-			  text-align: center;
-			}
-
-			p {
-			  font-family: verdana;
-			  font-size: 20px;
-		}
+			
+		
 	</style>
 </head>
 <body>
 
 <h1>歡迎光臨我的網站</h1>
-<hr>
-<p>有什麼問題歡迎連絡 minhuang@nkust.edu.tw</p>
+<button><a href ='20210323/'>Home</a></button>
+<button><a href ='http://cla.nkust.edu.tw'>博雅中心</a></button>
+<button><a href ='http://tzukuan.nkust.org'>計畫網站</a></button>
+<select onchange='location.href= "/20210323/"+this.value'>
+	<option value="0323-1.php">請選擇要執行練習</option>
+	<option value="0323-1.php">0323-1</option>
+	<option value="0323-2.php">0323-2</option>
+</select>
+
+<hr><hr>
+
+<p>有什麼問題歡迎連絡 0651103@nkust.edu.tw</p>
 
 </body>
 </html>
+
